ed02f11faf1fd21df97870fd0ce036d6b1537736
Alexey Zhokhov <alexey@zhokhov.com>
2019-08-01 00:30:02 +0800

    Some fixes.

--- build-docker.sh
+++ build-docker.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+ABSOLUTE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd "${ABSOLUTE_PATH}" || exit
+
+set -e
 
 PACKAGE="kube-swiss-knife"
 VERSION="latest"
-
-set -e
 
 echo "> 1. Building Docker image"
 echo ""
